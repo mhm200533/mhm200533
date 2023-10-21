dfc98665deafbb193224f9e030106814f69988c3
- <idlesauce@privatemail.com>
2023-10-22 01:26:51 +0200

    Add updated k-stuff with 4.50 support

--- document/en/ps5/payload_map.js
+++ document/en/ps5/payload_map.js
@@ -1,12 +1,12 @@
 const payload_map =
     [
         {
-            displayTitle: 'K-Stuff (FW 4.03 Only)',
-            description: 'Allows decryption/running of fpkg apps',
-            fileName: 'ps5-kstuff.bin',
+            displayTitle: 'K-Stuff',
+            description: 'FW 4.03 & 4.50 Only. FPKG enabler',
+            fileName: 'ps5-kstuff-v1.1.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
-            version: '1.0'
+            version: '1.1'
         },
         // {
         //     displayTitle: 'libhijacker game-patch (broken)',
