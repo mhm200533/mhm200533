35e275acfe29699fd647da4e53f45d720aec2085
- <idlesauce@privatemail.com>
2023-11-05 21:18:42 +0100

    Update host.py to not use deprecated ssl stuff

--- host.py
+++ host.py
@@ -16,11 +16,11 @@ class RequestHandler(SimpleHTTPRequestHandler):
         #print('!POST!: tn:\n'  + tn)
         fn = tn + '.bin' # '.json'
         if (not tn.startswith("T_")):
-        	if (fn!="a.bin"):
-        		print('!POST!: INFO: '  + str(self.rfile.read(int(self.headers['Content-length']))),"utf-8")
-        		return
-        	else:
-        		fn = time.strftime("%Y%m%d-%H%M%S") + ".json"
+            if (fn!="a.bin"):
+                print('!POST!: INFO: '  + str(self.rfile.read(int(self.headers['Content-length']))),"utf-8")
+                return
+            else:
+                fn = time.strftime("%Y%m%d-%H%M%S") + ".json"
 
         print('!POST!: ' + self.path + ' -->> ' + fn)
         print('test: %d'%int(self.headers['Content-length']))
@@ -29,7 +29,9 @@ class RequestHandler(SimpleHTTPRequestHandler):
 
 
 server_address = ('0.0.0.0', 443)
-httpd = HTTPServer(server_address, RequestHandler) #http.server.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='localhost.pem', ssl_version=ssl.PROTOCOL_TLS)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile='localhost.pem')
+httpd = http.server.HTTPServer(server_address, RequestHandler)
+httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 print('running server')
 httpd.serve_forever()
