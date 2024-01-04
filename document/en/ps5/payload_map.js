9ee789325aa47263c42382e1accef0fe1368c517
Echo Stretch <98502641+EchoStretch@users.noreply.github.com>
2024-01-03 19:08:05 -0700

    Update payload_map.js

--- document/en/ps5/payload_map.js
+++ document/en/ps5/payload_map.js
@@ -3,10 +3,10 @@ const payload_map =
         {
             displayTitle: 'etaHEN',
             description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 4.03-4.51 Only.',
-            fileName: 'etaHEN-1.2B.bin',
+            fileName: 'etaHEN-1.3B.bin',
             author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
             source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.2 beta'
+            version: '1.3 beta'
         },
         {
             displayTitle: 'K-Stuff',
